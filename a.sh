bad66f0a82f8915030793ca651aea04c89efd8b4
柒蓝 <86589703+qilan28@users.noreply.github.com>
2025-09-16 00:24:36 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -3,7 +3,7 @@ uname -m
 # apt-get update
 # apt install unzip 
 
-wget https://github.com/nezhahq/agent/releases/download/v0.17.6/nezha-agent_linux_arm64.zip
-unzip nezha-agent_linux_arm64.zip 
+wget https://github.com/nezhahq/agent/releases/download/v0.17.6/nezha-agent_linux_arm64.zip -O nezha-agent_linux_arm64.zip
+unzip nezha-agent_linux_arm64.zip
 
 ./nezha-agent -s tzz.282820.xyz:443 -p WcjQfoUyl6MyxzNUtA  --tls --debug --temperature --disable-force-update  --disable-auto-update 
